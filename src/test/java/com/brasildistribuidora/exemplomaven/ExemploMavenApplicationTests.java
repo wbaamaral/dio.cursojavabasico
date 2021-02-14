d71cfc8f609a1ffd1581d17e02ef187527d2bc3b
Wélyqrson de Bastos Amaral <pvh.ti@brasildistribuidora.com>
2021-02-14 17:56:16 -0400

    Commit inicial

---
+++ src/test/java/com/brasildistribuidora/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brasildistribuidora.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
